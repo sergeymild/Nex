ce8f6608eeb01cddeeefcf3b1f90b7f47c3efb2a
Sergeymild <sergeymild@wisebits.com>
2019-10-25 13:23:07 +0300

    remove logs

--- nex-plugin/src/main/java/com/nex/gradle/NexPlugin.kt
+++ nex-plugin/src/main/java/com/nex/gradle/NexPlugin.kt
@@ -10,7 +10,7 @@ import org.gradle.api.Project
 open class NexPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.dependencies.add("implementation", "com.nex:nex-library:1.0.12")
+        target.dependencies.add("implementation", "com.nex:nex-library:1.0.13")
 
         val android = target.extensions.findByName("android") as BaseExtension
         android.registerTransform(
